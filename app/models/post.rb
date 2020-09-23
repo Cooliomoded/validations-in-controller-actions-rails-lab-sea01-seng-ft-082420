72c5ade67e8fc6ce5eff3ff4d0b6da247480c66f
Cooliomoded <cooliomoded@gmail.com>
2020-09-23 16:59:26 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    
+    belongs_to :author
+
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(fiction non-fiction) }
+    validates :content, length: { minimum: 100 }
 end
